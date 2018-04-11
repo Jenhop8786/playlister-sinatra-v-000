60784469c6c9b67928df589877605b4bcc885d48
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-11 23:59:55 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,15 +1,12 @@
 class ArtistsController < ApplicationController
 
-  get '/artists' do
-    @artists = Artist.all
+    get '/artists' do
+        @artists = Artist.all
+        erb :'/artists/index'
+    end
 
-    erb :'artists/index'
-  end
-
-  get '/artists/:slug' do
-    @songs = Song.all
-    @artists = Artist.all
-
-    erb :'artists/show'
-  end
+    get'/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/show'
+    end
 end#ArtistsController
