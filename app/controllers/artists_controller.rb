731c3e60a4d5723c05827bbb09a2380abefaff74
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-11 23:35:39 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,15 @@
 class ArtistsController < ApplicationController
-end
+
+  get '/artists' do
+    @artists = Artist.all
+
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do
+    @songs = Song.all
+    @artists = Artist.all
+
+    erb :'artists/show'
+  end
+end 
